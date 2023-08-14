827c7fa24839d950fe691ae94448dc798cd3c384
Thayná <thaynavazzoler.564@gmail.com>
2023-08-14 16:11:53 -0300

    "feat: function"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,37 @@
-const tasks = [];
-
-function send(){
-    let taskInput = document.getElementById('type Task'); let texto = taskInput.value
+class Task {
+    constructor(id, title, status){
+        this.id = id;
+        this.title = title;
+        this.status = status;
+    }
 }
+
+class TaskList{
+    constructor(){
+        this.tasks = [];
+    }
+    addTask(param){
+        this.tasks.push(param);
+    }
+}
+const veryBigTaskList = new TaskList();
+
+function createTask(){
+    const taskTitle = document.getElementById("new Task").value;
+
+    const task = new Task(1, taskTitle,false);
+
+    veryBigTaskList.addTask(task);
+    console.log(veryBigTaskList);
+}
+
+function renderTasks(){
+    let element = "";
+    veryBigTaskList.tasks.forEach(task =>{
+        
+    }
+
+    )
+}
+
+
